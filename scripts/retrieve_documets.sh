4c856d3f4850388a3d39ced419b0d517e3bfb423
h3inkr <minjuhong@korea.ac.kr>
2025-05-02 08:43:08 +0900

    Delete scripts/retrieve_documets.sh

--- scripts/retrieve_documets.sh
+++
@@ -1,5 +0,0 @@
-python /home/user4/Speculative_RAG/src/retrieve.py \
-    --jsonl_path /home/user4/Speculative_RAG/data/inference/csqa/csqa-dev.jsonl \
-    --output_path /home/user4/Speculative_RAG/data/inference/csqa/csqa-dev_retrieved.jsonl \
-    --load_index_path ./data/inference/raco.index \
-    --load_metadata_path ./data/inference/raco_meta.pkl
